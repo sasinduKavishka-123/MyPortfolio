dc5b53e2e75cec62ba19e6c76af21497efd8d051
sasinduKavishka-123 <sasindu.chemistry@gmail.com>
2026-04-08 22:42:24 +0530

    develop  globe

--- JS/globe.js
+++ JS/globe.js
@@ -4,29 +4,26 @@ import createGlobe from 'https://cdn.skypack.dev/cobe';
 
 const canvas = document.getElementById("globeCanvas");
 const container = document.querySelector('#gridSection4GlobeHolder');
-let phi = 0;
 
+const globe = createGlobe(canvas, {
+    devicePixelRatio: 2,
+    width: container.clientWidth * 2,
+    height: container.clientWidth * 2,
+    phi: 0,
+    theta: 0.2,
+    dark: 1.1,
+    diffuse: 1.4,
+    mapSamples: 34000,
+    mapBrightness: 6,
+    baseColor: [0.6, 0.4, 0.8],
+    glowColor: [0.6, 0.6, 0.6],
+})
 
-// noinspection JSUnusedGlobalSymbols
-const globe = createGlobe(canvas,
-    {
-        devicePixelRatio: 2,
-        width: 1000,
-        height: 1000,
-        phi: 0,
-        theta: 0,
-        dark: 1,
-        diffuse: 1.2,
-        mapSamples: 16000,
-        mapBrightness: 18,
-        baseColor: [0.1, 0.1, 0.1],
-        markerColor: [1, 0, 0],
-        glowColor: [1, 1, 1],
-        onRender: (state) => {
-            state.phi = phi;
-            phi += 0.01;
-        }
-    });
-
-// globe.toggle();
-
+// Animate the globe
+let phi = 0
+function animate() {
+    phi += 0.005
+    globe.update({ phi })
+    requestAnimationFrame(animate)
+}
+animate()
