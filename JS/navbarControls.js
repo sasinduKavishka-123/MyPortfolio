67f1ea47a99b7fae6f3362caeda0a48665621dd2
sasinduKavishka-123 <sasindu.chemistry@gmail.com>
2026-04-25 22:50:16 +0530

    add navbar icon anims

--- JS/navbarControls.js
+++ JS/navbarControls.js
@@ -6,6 +6,8 @@ let closeIcon = document.getElementById('sidebarCloseIcon');
 
 closeIcon.addEventListener('click', ()=>{
     sidebar.style.animation = 'slidOutRight 0.7s ease-out';
+    barIcon.style.display = 'inline-block';
+    barIcon.style.animation = 'slidInRightNavIcon 0.7s ease-out';
     setTimeout(()=>{
         sidebar.style.display = 'none';
     },700);
@@ -14,4 +16,8 @@ closeIcon.addEventListener('click', ()=>{
 barIcon.addEventListener('click', ()=>{
     sidebar.style.display = 'flex';
     sidebar.style.animation = 'slidInRight 0.7s ease-out';
+    barIcon.style.animation = 'slidOutRightNavIcon 0.7s ease-out';
+    setTimeout(()=>{
+        barIcon.style.display = 'none';
+    },700);
 });
